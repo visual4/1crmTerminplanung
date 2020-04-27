7fe7b34d40a3630ab03fe35dcbbb3b4e7b558292
brafreider <bjoern_rafreider@visual4.de>
2020-04-27 20:16:15 +0200

    Version angepasst, Kommentar entfernt

--- manifest.php
+++ manifest.php
@@ -3,11 +3,12 @@ acceptable_sugar_versions
     regex_matches
         - 7\.[7-8]\.[0-9][0-9]?[a-z]?
         - 8\.0\.[0-9][0-9]?
+        - 8\.5\.[0-9][0-9]?
 name: 1CRM Erweiterungen terminabgleich
 description: 1CRM Erweiterungen fuer terminabgleich
 author: visual4 GmbH
-published_date: 2017-11-12
-version: 1.8.0.10-179
+published_date: 2020-04-27
+version: 1.8.0.10-180
 type: module
 is_uninstallable: true
 id: v4terminabgleich
--- root/modules/v4Terminabgleich/v4Terminabgleich.php
+++ root/modules/v4Terminabgleich/v4Terminabgleich.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: brafreider
- * Date: 12.11.2017
- * Time: 12:33
- */
-
 class v4Terminabgleich extends SugarBean
 {
 
